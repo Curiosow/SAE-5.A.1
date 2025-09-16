476e374b029eef0384e17412d4461a6d96a1c8d1
Curiosow <teampandapro45@gmail.com>
2025-09-16 15:17:26 +0200

    add tailwind on all pages and components

--- SAE-5.A.1/tailwind.config.js
+++ SAE-5.A.1/tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./src/components/**/*.{js,vue,ts}",
+    "./src/layouts/**/*.vue",
+    "./src/pages/**/*.vue",
+    "./src/plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
